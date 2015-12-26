e20d47db32b2773c18ae0115dade7d21325eca5d
iceiony <iceiony@yahoo.com>
2015-12-26 20:51:49 +0000

    load assets and determine first player to start

--- NAC.m
+++ NAC.m
@@ -1,1 +1,17 @@
-gamePreferences = askpreferences();
+clear all;
+close all;
+
+Game.Preferences = askpreferences();
+
+if  ~isstruct(Game.Preferences) 
+    disp('Game Ended since no choice made');
+    return; 
+end
+
+%select the first player to start randomly 
+Game.CurrentPlayer = randi(2);
+Game.Bord = zeros(Game.Preferences.Size(1),Game.Preferences.Size(2));
+
+[bordImg,oImg,xImg] = loadassets(Game.Preferences); 
+
+
---
+++ loadassets.m
@@ -0,0 +1,12 @@
+function [ bordImg, oImg, xImg] = loadassets( gamePreferences )
+    %loads the asset images based on user selected preferences ( bord size ) 
+    PATH = './images';
+    
+    rows = gamePreferences.Size(1);
+    columns = gamePreferences.Size(2);
+    
+    bordImg = imread(sprintf('%s/g%dx%d.bmp',PATH,rows,columns));
+    oImg = imread([PATH '/sO.bmp']);
+    xImg = imread([PATH '/sX.bmp']);
+end
+
