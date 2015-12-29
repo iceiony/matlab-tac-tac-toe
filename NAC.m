7268640dfc0423ba061d0cb2b8b8809676d82f0e
iceiony <iceiony@yahoo.com>
2015-12-29 01:31:45 +0000

    refactor playing one game in a separate function file

--- NAC.m
+++ NAC.m
@@ -9,39 +9,8 @@ end
 
 game = initialisegame(preferences);
 
-%play one game
-game.IsEnded = false;
-game.CurrentBoardImg = game.BoardImg;
-
-axis equal;
-imshow(game.CurrentBoardImg);
-axis equal;
-
-while(~game.IsEnded)
-    if preferences.PlayerIsComputer(game.CurrentPlayer)
-        choice = randomchoice(game);
-    else
-        choice = askplayerchoice(game);
-        if isempty(choice)
-            disp('Game ended window closed');
-            return;
-        end
-    end
-    
-    [game.Board,game.CurrentBoardImg] = updateboard(choice, game);
-    
-    [game.IsEnded,winningCells] = iswinning(choice,game);
-    if game.IsEnded
-        showwin(winningCells,game);
-        xlabel(sprintf('%s wins!',preferences.PlayerNames{game.CurrentPlayer}));
-        return;
-    elseif ~any(find(game.Board == 0))
-        game.IsEnded  = true;
-        xlabel('Game ended tie!')
-        return;
-    end
-    
-    %switch to next player
-    game.CurrentPlayer = mod(game.CurrentPlayer,2) + 1; 
-    xlabel(sprintf('%s''s turn',preferences.PlayerNames{game.CurrentPlayer}));
+winner = playonegame(game,preferences);
+if ~isnan(winner)
+    game.Score(winner) = game.Score(winner)+1;
 end
+game.CurrentPlayer = mod(game.CurrentPlayer,2) +1;
---
+++ playonegame.m
@@ -0,0 +1,41 @@
+function [winner] = playonegame(game,preferences)
+%play one game and return the winner
+    game.IsEnded = false;
+    game.CurrentBoardImg = game.BoardImg;
+
+    axis equal;
+    imshow(game.CurrentBoardImg);
+    axis equal;
+
+    while(~game.IsEnded)
+        if preferences.PlayerIsComputer(game.CurrentPlayer)
+            choice = randomchoice(game);
+        else
+            choice = askplayerchoice(game);
+            if isempty(choice)
+                disp('Game ended window closed');
+                winner = nan;
+                return;
+            end
+        end
+
+        [game.Board,game.CurrentBoardImg] = updateboard(choice, game);
+
+        [game.IsEnded,winningCells] = iswinning(choice,game);
+        if game.IsEnded
+            showwin(winningCells,game);
+            xlabel(sprintf('%s wins!',preferences.PlayerNames{game.CurrentPlayer}));
+            winner = game.CurrentPlayer;
+            return;
+        elseif ~any(find(game.Board == 0))
+            game.IsEnded  = true;
+            xlabel('Game ended tie!')
+            winner = nan;
+            return;
+        end
+
+        %switch to next player
+        game.CurrentPlayer = mod(game.CurrentPlayer,2) + 1; 
+        xlabel(sprintf('%s''s turn',preferences.PlayerNames{game.CurrentPlayer}));
+    end
+end
